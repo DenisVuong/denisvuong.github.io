87f2d6148761de982481553164aff3774e4b0922
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-02-27 11:54:38 +0700

    first import

---
+++ Controller/navbar.php
@@ -0,0 +1,7 @@
+<nav class="nav">
+    <ul class="menu">
+        <li><a href="#">Infos</a></li>
+        <li><a href="#">Projet</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
---
+++ Controller/parcours.php
@@ -0,0 +1,39 @@
+<div class="edu-section">
+    <h2 class="edu-title">Education</h2>
+    <div class="edu-timeline">
+        <div class="edu-item">
+            <div class="edu-left">
+                <div class="edu-date">2020-2023</div>
+                <p class="edu-location">Lycée René Cassin<br>Gonesse, France</p>
+            </div>
+            <div class="edu-middle">
+                <div class="edu-circle"></div>
+            </div>
+            <div class="edu-right">
+                <h3 class="edu-degree">Bac Général</h3>
+                <p class="edu-desc">Bac générale mention assez bien</p>
+                <p class="edu-spec">Spécialité :</p>
+                <ul class="edu-list">
+                    <li>Mathématiques</li>
+                    <li>Numérique Science Informatique</li>
+                    <li>LLCER Anglais</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="edu-item">
+            <div class="edu-left">
+                <div class="edu-date">2023-2026</div>
+                <p class="edu-location">Sorbonne Paris Nord<br>Villetaneuse, France</p>
+            </div>
+            <div class="edu-middle">
+                <div class="edu-circle"></div>
+            </div>
+            <div class="edu-right">
+                <h3 class="edu-degree">BUT INFORMATIQUE</h3>
+                <p class="edu-desc">Parcours réalisation d’applications : conception, développement, validation</p>
+                <p class="edu-status">Première année validée</p>
+            </div>
+        </div>
+    </div>
+</div>
