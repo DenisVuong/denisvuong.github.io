183b604520047ade8dbd13a3421f670291fd6c38
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-03-06 13:19:00 +0700

    delete all php

--- Controller/mailForm.php
+++
@@ -1,9 +0,0 @@
-<div class="Mailcontainer">
-    <h1>Formulaire de contact</h1>
-    <form action="https://formsubmit.co/vuong.denis.p@gmail.com" method="POST" class="form">
-        <input type="text" name="Name" placeholder="Nom complet" required>
-        <input type="email" name="email" placeholder="Adresse email" required>
-        <textarea name="Message" placeholder="Message" required></textarea>
-        <button type="submit">Envoyer</button>
-    </form>
-</div>
