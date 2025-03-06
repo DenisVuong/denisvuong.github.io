75b50166b678c42f9067c33b64cb3f08436652e6
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-03-06 11:55:26 +0700

    Delete Controller directory

--- Controller/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav class="nav">
-    <ul class="menu">
-        <li><a href="#">Infos</a></li>
-        <li><a href="#">Projet</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-</nav>
