8853703c3bf62eb374de34e9b5703b30d083d604
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-03-06 13:17:13 +0700

    site

---
+++ Controller/card.php
@@ -0,0 +1,84 @@
+<div class="popup" id="popup-1">
+    <div class="overlay"></div>
+    <div class="content">
+        <div class="close-btn" onclick="togglePopup1()">&times;</div>
+        <h3>Organisation d'un travail d'equipe</h3>
+        <p>- Création d’un site web pour un événement culturel en 2025, avec intégration de médias et accessibilité en anglais.
+        <br>
+        - Participation sur le terrain : interviews des participants pour enrichir le contenu du site.
+        <br>
+        - Développement de compétences en travail d’équipe, gestion de projet, communication et journalisme.
+        </p>
+        <div class="appli-pop">
+        <img src="../img/logoSkills/logo skill w/html5-brands.png" alt="HTML">
+        <img src="../img/logoSkills/logo skill w/css3-alt-brands.png" alt="CSS">
+        <img src="../img/logoSkills/logo skill w/js-brands.png" alt="JavaScript">
+        <img src="../img/Adobe_Photoshop_CC_icon 1.png" alt="Photoshop">
+        </div>
+    </div>
+</div>
+
+<div class="popup" id="popup-2">
+    <div class="overlay"></div>
+    <div class="content">
+        <div class="close-btn" onclick="togglePopup2()">&times;</div>
+        <h3>Développement d'une application</h3>
+        <p>- Développement en équipe d’un site web pour la gestion des stages à l’université, avec intégration d’une base de données et d’un système de cryptage.
+            <br>
+        - Collaboration efficace, renforçant mes compétences en travail d’équipe, développement web et sécurité informatique.</p>
+        <div class="appli-pop">
+        <img src="../img/logoSkills/logo skill w/html5-brands.png" alt="HTML">
+        <img src="../img/logoSkills/logo skill w/css3-alt-brands.png" alt="CSS">
+        <img src="../img/logoSkills/logo skill w/js-brands.png" alt="JavaScript">
+        <img src="../img/logoSkills/logo skill w/Official PHP Logo 1.png" alt="PHP">
+        <img src="../img/logoSkills/logo skill w/database-solid.png" alt="SQL">
+        <img src="../img/Figma 1.png" alt="Figma">
+    </div>
+</div>
+</div>
+
+
+<div class="popup" id="popup-3">
+    <div class="overlay"></div>
+    <div class="content">
+        <div class="close-btn" onclick="togglePopup3()">&times;</div>
+        <h3>Recueil de besoins</h3>
+        <p>- Échange avec un client fictif afin de recueillir les besoins du site.
+            <br>
+            - Réalisation d’un maquette sur Figma, ajusté selon les retours client.</p>
+        <div class="appli-pop">
+        <img src="../img/Figma 1.png" alt="Figma">
+        <img src="../img/Adobe_Photoshop_CC_icon 1.png" alt="Photoshop">
+        
+    </div>
+</div>
+</div>
+
+
+<!-----> 
+
+<div class="container_card">
+
+    <div class="card">
+        <img class="card-img" src="../img/Affiche_Concours_d_élégance 1.png" alt="Concours d'élégance">
+        <h2>Organisation d'un travail d'equipe</h2>
+        <button onclick="togglePopup1()">Voir plus  →</button>
+    </div>
+
+    <div class="card">
+        <img class="card-img" src="../img/image 2.png" alt="Site gestion de stage">
+        <h2>Développement d'une application</h2>
+        <button onclick="togglePopup2()">Voir plus  →</button>
+    </div>
+
+    <div class="card">
+        <img class="card-img" src="../img/Capture d’écran 2025-03-05 à 10.49.22 1.png" alt="site bar jazz">
+        <h2>Recueil de besoins</h2>
+        <button onclick="togglePopup3()">Voir plus  →</button>
+    </div>
+
+
+</div>
+
+<script src="../js/popup.js"></script>
+
---
+++ Controller/navbar.php
@@ -0,0 +1,15 @@
+<nav class="nav">
+    <ul class="menu">
+        <li><a href="#one">Infos</a></li>
+        <li><a href="#two">Projet</a></li>
+        <li><a href="#three">Contact</a></li>
+    </ul>
+    
+    <div class="lang">
+       <select name="" id="">
+            <option value="fr">🇫🇷 Français</option>
+            <option value="en">🇺🇸 English</option>
+            <option value="viet">🇻🇳 Tiếng Việt</option>
+       </select>
+    </div>
+</nav>
